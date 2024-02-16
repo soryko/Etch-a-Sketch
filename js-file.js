9a18263a41d6171358189226b23468b8bac02310
Sory <sorykora@gmail.com>
2024-02-16 18:53:39 +0100

    added a poupwindow

--- js-file.js
+++ js-file.js
@@ -1,9 +1,18 @@
 const items = document.querySelectorAll('.item');
 
-function changeBackgroundColor(){
-    items.style.backgroundColor = 'white';
+function changeBackgroundColor(event){
+    event.target.style.backgroundColor = 'white';
 }
-
+function chooseGrid(input=4){
+    if (input<=100){
+        document.body.container.style.gridTemplateColumns = `repeat(${input}, 1fr)`;
+        document.body.container.style.gridTemplateRows = `repeat(${input}, 1fr)`;
+    }
+}
+function popUpAction(){
+    let popup = document.getElementById("myPopup");
+    popup.classList.toggle('show');
+}
 for (const item of items){
-    items.addEventListener("mouseover", changeBackgroundColor);
+    item.addEventListener("mouseover", changeBackgroundColor);
 }
